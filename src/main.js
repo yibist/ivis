f36e922461d8fbb5de6eed922c07b5951b30c648
val94-s <valerie.scheidegger1@students.fhnw.ch>
2026-04-21 15:01:36 +0200

    struggling with parsing the data

--- src/main.js
+++ src/main.js
@@ -1,14 +1,32 @@
-// main.js
 import * as d3 from "d3";
-import fs from "fs";
 
-// Read CSV file from disk
-const csvFile = "./resources/data/banlist.csv";
-const csvData = fs.readFileSync(csvFile, "utf-8");
+
+const width = 1000;
+const height = 600;
+const margin = {top: 50, right: 20, bottom: 50, left: 200};
+
+
+/*const svg = d3.select("body")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);*/
+
+
+
+// Load CSV file
+d3.csv("./src/resources/data/banlist_1.csv").then(data => {
+    console.log(data);
+});
+
 
 // Parse CSV into array of objects
-const data = d3.csvParse(csvData, d => ({
-    CardName: d.CardName,
-}));
 
-console.log(data);
+
+//
+
+
+
+
+
+
+
