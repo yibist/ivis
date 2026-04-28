8524f395987a4d964301c16adfa7e8b6f1e0bb87
val94-s <valerie.scheidegger1@students.fhnw.ch>
2026-04-28 12:29:17 +0200

    no longer hard coded to just one card, but can produce values of multiple cards via dropdown window

--- src/main.js
+++ src/main.js
@@ -1,62 +1,94 @@
-// set the dimensions and margins of the graph
 const margin = {top: 10, right: 30, bottom: 30, left: 40},
     width = 460 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
 
-
-// append the svg object to the body of the page
 const svg = d3.select("#my_dataviz")
     .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
-    .attr("transform",
-        "translate(" + margin.left + "," + margin.top + ")");
+    .attr("transform", `translate(${margin.left},${margin.top})`);
 
-// Load CSV file
 d3.csv("src/resources/data/banlist_1.csv").then(function(data) {
 
-    // pick one card
-    const card = data.find(d => d["Card Name"] === "A Hero Lives");
+    // -----------------------
+    // Dropdown
+    // -----------------------
+    const cardNames = data.map(d => d["Card Name"]);
 
-    // convert row → array of {date, value}
-    const values = Object.keys(card)
-        .filter(k => k !== "Card Name")
-        .map(k => ({
-            date: d3.timeParse("%Y-%m")(k),
-            value: +card[k]
-        }));
+    d3.select("#cardSelect")
+        .selectAll("option")
+        .data(cardNames)
+        .enter()
+        .append("option")
+        .text(d => d)
+        .attr("value", d => d);
 
-    // x scale (time instead of linear)
-    const x = d3.scaleTime()
-        .domain(d3.extent(values, d => d.date))
-        .range([0, width]);
+    // -----------------------
+    // Scales
+    // -----------------------
+    const x = d3.scaleTime().range([0, width]);
+    const y = d3.scaleLinear().domain([-1, 3]).range([height, 0]);
 
-    svg.append("g")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x));
+    const xAxis = svg.append("g").attr("transform", `translate(0,${height})`);
+    const yAxis = svg.append("g").call(d3.axisLeft(y));
 
-    // y scale (ban status)
-    var y = d3.scaleLinear()
-        .domain([-1, 3])
-        .range([height, 0]);
-
-    svg.append("g")
-        .call(d3.axisLeft(y));
-
-    // line generator
-    const line = d3.line()
-        .x(d => x(d.date))
-        .y(d => y(d.value));
-
-    svg.append("path")
-        .datum(values)
+    // Line path (empty at start)
+    const linePath = svg.append("path")
         .attr("fill", "none")
         .attr("stroke", "#69b3a2")
-        .attr("stroke-width", 2)
-        .attr("d", line);
+        .attr("stroke-width", 2);
+
+    const parseDate = d3.timeParse("%Y-%m");
+
+    // -----------------------
+    // Update function
+    // -----------------------
+    function update(selectedCard) {
+
+        const card = data.find(d => d["Card Name"] === selectedCard);
+
+        const values = Object.keys(card)
+            .filter(k => k !== "Card Name")
+            .map(k => ({
+                date: parseDate(k),
+                value: +card[k]
+            }));
+
+        // Update scales
+        x.domain(d3.extent(values, d => d.date));
+        xAxis.call(d3.axisBottom(x));
+
+        // Update line
+        const line = d3.line()
+            .x(d => x(d.date))
+            .y(d => y(d.value));
+
+        linePath
+            .datum(values)
+            .transition()
+            .duration(500)
+            .attr("d", line);
+    }
+
+    // -----------------------
+    // Initial draw
+    // -----------------------
+    update(cardNames[0]);
+
+    // -----------------------
+    // Event listener
+    // -----------------------
+    d3.select("#cardSelect").on("change", function() {
+        const selected = d3.select(this).property("value");
+        update(selected);
+    });
 
 });
+
+
+
+
 
 
 
